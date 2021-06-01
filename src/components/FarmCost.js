74542925ab3b94322fe775348746b3346013dc9e
hoangngocbaominh <hoangngocbaominh1998@gmail.com>
2021-06-01 10:01:00 +0700

    add

--- src/components/FarmCost.js
+++ src/components/FarmCost.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Row } from 'antd';
 FarmCost.propTypes = {
-    
+
 };
 
 function FarmCost(props) {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Row>
+                Chi phí của trang trại cơ bản
+        </Row>
+        <Row>
+                Chọn số lượng quản lý mỗi tháng của bạn
+        </Row>
+        </>
+
     );
 }
 
